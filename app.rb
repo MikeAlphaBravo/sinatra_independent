c9587c5812c4c8f78dc5cfa6c24929354c1efe18
Michael Brooks <silverfox@Michaels-MacBook-Pro.local>
2017-09-17 10:41:48 -0700

    Add hash working sytax for basic setup.

--- app.rb
+++ app.rb
@@ -1,20 +1,17 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
-require('./lib/word')
-require('./lib/define')
+require('./lib/dictionary')
 require('pry')
 
 get('/') do
   if Dictionary.all.empty?
     Dictionary.add_or_update("word1","definition1")
+    Dictionary.add_or_update("word2","definition1")
     Dictionary.add_or_update("word2","definition2")
-    Dictionary.add_or_update("word3","definition3")
   end
 
   @list = Dictionary.all()
-  binding.pry
-  
   erb(:input)
 end
 
@@ -24,14 +21,16 @@ post('/') do
   erb(:input)
 end
 
-get('/words/:word') do
+get('/words/:word?') do
+  binding.pry
   @word = Dictionary.find(params[:word])
+  # binding.pry
   erb(:output)
 end
 
 post('/words/:word') do
   @word = Dictionary.find(params[:word])
-  @word.additional_definitionextra({:define=>params["define"]})
+  @word.add_or_update({:definition=>params["definition"]})
   erb(:output)
 end
 
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,5 +1,5 @@
 require('rspec')
-require('word')
+require('dictionary')
 require('pry')
 
 describe("Word") do
